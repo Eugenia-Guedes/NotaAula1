b65ef1a08f5085d405a19339a0b3266d9599de5f
Eugenia-Guedes <144943984+Eugenia-Guedes@users.noreply.github.com>
2024-10-07 20:01:17 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,44 +4,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Produto produto = new Produto();
+        System.out.print("Digite o nome do aluno: ");
+        String nome = scanner.nextLine();
 
-        System.out.print("Digite o código do produto: ");
-        produto.setCodigo(scanner.nextLine());
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
 
-        System.out.print("Digite o nome do produto: ");
-        produto.setNome(scanner.nextLine());
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
 
-        System.out.print("Digite o tamanho/peso do produto: ");
-        produto.setTamanhoOuPeso(scanner.nextLine());
+        System.out.print("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
 
-        System.out.print("Digite a cor do produto: ");
-        produto.setCor(scanner.nextLine());
+        Aluno aluno = new Aluno();
 
-        System.out.print("Digite o valor do produto: R$ ");
-        produto.setValor(scanner.nextDouble());
+        aluno.setNome(nome);
+        aluno.setNota1(nota1);
+        aluno.setNota2(nota2);
+        aluno.setNota3(nota3);
 
-        System.out.print("Digite a quantidade em estoque: ");
-        produto.setQuantidadeEmEstoque(scanner.nextInt());
-
-        System.out.println("\nIniciando venda...");
-        System.out.print("Digite a quantidade que desejs vender: ");
-        int quantidadeVendida = scanner.nextInt();
-
-        scanner.nextLine();
-
-        System.out.print("Digite o método de pagamento (Pix, Espécie, Transferência, Débito, Crédito): ");
-        String metodoPagamento = scanner.nextLine();
-
-        boolean vendaRealizada = produto.realizarVenda(quantidadeVendida, metodoPagamento);
-
-        if (vendaRealizada) {
-            System.out.println("Venda realizada com sucesso.");
-            System.out.println("Quantidade restante em estoque: " + produto.getQuantidadeEmEstoque());
-        } else {
-            System.out.println("Venda não realizada.");
-        }
+        System.out.println("A média do(a) aluno(a) " + aluno.getNome() + " é: " + aluno.getMedia());
+        System.out.println("Situação: " + aluno.situacao());
 
         scanner.close();
+        }
     }
-}
